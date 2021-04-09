f5d41d93f6a97e389fd1b16e665deb019bfc8ee4
anshika1709 <awasthi.anshika1709@gmail.com>
2021-04-09 13:06:16 +0530

    Add get API for reponses

--- controller/questionnaire.js
+++ controller/questionnaire.js
@@ -1,5 +1,10 @@
 const Questionnaire = require('../models/questionnaire');
 
+module.exports.index = async(req, res) => {
+    const allAnswers = await Questionnaire.find({});
+    console.log(allAnswers);
+
+}
 
 module.exports.renderNewForm = async(req, res) => {
     res.render("questionnaire/new");
@@ -10,7 +15,9 @@ module.exports.createQuestionnaire = async(req, res) => {
     await answers.save();
 
     console.log(answers);
-    //  res.redirect(`campgrounds/${newCampground._id}`);
+
+    //TODO redirect on graphs page may be!
+    //  res.redirect(``);
 
 
 }
