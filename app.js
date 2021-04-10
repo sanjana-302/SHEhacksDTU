759369767f238b503e7bad06c6f9277d16660e0b
anshika1709 <awasthi.anshika1709@gmail.com>
2021-04-10 11:04:06 +0530

    Add user login and register

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+const User = require('./models/user');
 const Questionnaire = require('./models/questionnaire');
 const questionnaireRoutes = require('./routes/questionnaire');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const userRoutes = require('./routes/users');
 
 mongoose.connect('mongodb://localhost:27017/vaccine', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
@@ -19,17 +26,44 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+const sessionConfig = {
+    name: 'session',
+    secret: "Why",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/', userRoutes);
+app.use('/questionnaire', questionnaireRoutes)
 
 app.get('/', function(req, res) {
     res.render('home');
 });
-
-
-app.use('/questionnaire', questionnaireRoutes)
-
-
-
-
 
 app.listen(3000, () => {
     console.log("Serving");
